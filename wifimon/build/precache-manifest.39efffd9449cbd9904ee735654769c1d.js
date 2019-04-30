f314f727d8ec258700c7d19317b6e1869ef7bd94
Adam Price <adam.price@shopify.com>
2019-04-30 11:59:05 +0100

    initial commit

---
+++ wifimon/build/precache-manifest.39efffd9449cbd9904ee735654769c1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ee924d04256ff50652177f758ed64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74840c04f5691283c447",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "f1aa6089a9b315c689a1",
+    "url": "/static/js/2.f3034c4d.chunk.js"
+  },
+  {
+    "revision": "74840c04f5691283c447",
+    "url": "/static/js/main.6499f030.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
